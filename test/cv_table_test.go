6488a640b289078e1c67a6a86fbfafd26a94e9bf
Leander Gmeiner <leander.gmeiner@gmail.com>
2024-01-19 22:41:52 +0100

    Testing: Assert that an attendance sheet has exactly 3 columns.

---
+++ test/cv_table_test.go
@@ -0,0 +1,25 @@
+package test
+
+import (
+	"testing"
+
+	"github.com/DHBW-SE-2023/YAAC/internal/cv"
+	"gocv.io/x/gocv"
+)
+
+func TestTableColumnCount(t *testing.T) {
+	attendanceListPath := "../data/list.jpg"
+	img := gocv.IMRead(attendanceListPath, gocv.IMReadAnyColor)
+
+	img = cv.FindTable(img)
+	table := cv.NewTable(img)
+
+	for idx, row := range table.Rows {
+		// A standard attendance list always has 3 columns
+		l := len(row)
+		if l != 3 {
+			t.Fatalf("The row %v is expected to have 3 columns, but it has %v", idx, l)
+		}
+	}
+
+}
