5b5182c7ada9a7834af67c3aa17df0b243a64e56
Leander Gmeiner <leander.gmeiner@gmail.com>
2024-01-19 16:18:40 +0100

    Preprocess an image to ease further processing

--- internal/cv/pipeline.go
+++ internal/cv/pipeline.go
@@ -1,1 +1,17 @@
 package cv
+
+import (
+	"image"
+
+	"gocv.io/x/gocv"
+)
+
+func FindTable(img gocv.Mat) gocv.Mat {
+	gocv.CvtColor(img, &img, gocv.ColorBGRToGray)
+	gocv.GaussianBlur(img, &img, image.Point{X: 3, Y: 3}, 2.0, 0.0, gocv.BorderDefault)
+	gocv.Threshold(img, &img, 128.0, 255.0, gocv.ThresholdOtsu)
+	gocv.FastNlMeansDenoisingWithParams(img, &img, 11.0, 31, 9)
+	gocv.Canny(img, &img, 50.0, 150.0)
+
+	return img
+}
