09c4987cabeb1e030edf01ec93122eb0d637caea
Vinzent <vinzent.engel@gmx.de>
2024-02-02 19:33:14 +0100

    update New() backend mail

--- internal/backend/mail/mvvm.go
+++ internal/backend/mail/mvvm.go
@@ -1,14 +1,35 @@
 package yaac_backend_mail
 
+import "time"
+
 type mvvm interface {
 }
 
 type BackendMail struct {
-	MVVM mvvm
+	MVVM       mvvm
+	serverAddr string
+	username   string
+	password   string
 }
 
-func New(mvvm mvvm) *BackendMail {
-	return &BackendMail{
-		MVVM: mvvm,
+type MailData struct {
+	image_data []byte
+	course     string
+	datetime   time.Time
+}
+
+// try to create a new backend_mail struct
+// returns an error if it is not possible to connect to the server
+func New(mvvm mvvm, serverAddr string, username string, password string) (*BackendMail, error) {
+	mailservice := BackendMail{
+		MVVM:       mvvm,
+		serverAddr: serverAddr,
+		username:   username,
+		password:   password,
 	}
+	_, _, err := mailservice.setupMail()
+	if err != nil {
+		return nil, err
+	}
+	return &mailservice, nil
 }
