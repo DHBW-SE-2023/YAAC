2f32219976a845659206a1138259978d22b6a365
Leander Gmeiner <leander.gmeiner@gmail.com>
2024-03-07 20:01:28 +0100

    Extracted single mail processing logic into its own function.

--- internal/backend/demon/demon.go
+++ internal/backend/demon/demon.go
@@ -17,51 +17,10 @@ func StartDemon(mvvm shared.MVVM, duration time.Duration) {
 		}
 
 		for _, mail := range newMails {
-			table, err := mvvm.ValidateTable(mail.Image)
+			list, err := TableToAttendanceList(mvvm, mail)
 			if err != nil {
 				log.Fatalf("Could not process image from mail received at %v", mail.ReceivedAt)
 				continue
-			}
-
-			list := shared.AttendanceList{
-				ReceivedAt: mail.ReceivedAt,
-				Image:      mail.Image,
-			}
-
-			// TODO: Get the course from the table header
-			for _, row := range table.Rows {
-				students, err := mvvm.Students(shared.Student{FirstName: row.FirstName, LastName: row.LastName})
-				if err != nil {
-					continue
-				}
-
-				var student shared.Student
-
-				// TODO: Is this correct: No student with the name is present. Consider adding them
-				if len(students) == 0 {
-					student, err = mvvm.InsertStudent(shared.Student{
-						FirstName: row.FirstName,
-						LastName:  row.LastName,
-					})
-
-					if err != nil {
-						continue
-					}
-				} else if len(students) != 1 { // If there are more students with the same name, we don't know what to do
-					continue
-				}
-
-				student = students[0]
-
-				attendance := shared.Attendance{
-					StudentID:    student.CourseID,
-					IsAttending:  row.Valid,
-					NameROI:      row.NameROI,
-					SignatureROI: row.SignatureROI,
-					TotalROI:     row.TotalROI,
-				}
-
-				list.Attendancies = append(list.Attendancies, attendance)
 			}
 
 			_, err = mvvm.InsertList(list)
@@ -75,4 +34,54 @@ func StartDemon(mvvm shared.MVVM, duration time.Duration) {
 
 		time.Sleep(duration * time.Second)
 	}
+}
+
+func TableToAttendanceList(mvvm shared.MVVM, mail shared.MailData) (shared.AttendanceList, error) {
+	table, err := mvvm.ValidateTable(mail.Image)
+	if err != nil {
+		return shared.AttendanceList{}, err
+	}
+
+	list := shared.AttendanceList{
+		ReceivedAt: mail.ReceivedAt,
+		Image:      mail.Image,
+	}
+
+	// TODO: Get the course from the table header
+	for _, row := range table.Rows {
+		students, err := mvvm.Students(shared.Student{FirstName: row.FirstName, LastName: row.LastName})
+		if err != nil {
+			continue
+		}
+
+		var student shared.Student
+
+		// TODO: Is this correct: No student with the name is present. Consider adding them
+		if len(students) == 0 {
+			student, err = mvvm.InsertStudent(shared.Student{
+				FirstName: row.FirstName,
+				LastName:  row.LastName,
+			})
+
+			if err != nil {
+				continue
+			}
+		} else if len(students) != 1 { // If there are more students with the same name, we don't know what to do
+			continue
+		}
+
+		student = students[0]
+
+		attendance := shared.Attendance{
+			StudentID:    student.CourseID,
+			IsAttending:  row.Valid,
+			NameROI:      row.NameROI,
+			SignatureROI: row.SignatureROI,
+			TotalROI:     row.TotalROI,
+		}
+
+		list.Attendancies = append(list.Attendancies, attendance)
+	}
+
+	return list, nil
 }
