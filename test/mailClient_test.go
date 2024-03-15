16cb4c552394ad90654d53704f7400bd031bff98
Vinzent <vinzent.engel@gmx.de>
2024-03-15 11:49:19 +0100

    add testsMail

---
+++ test/mailClient_test.go
@@ -0,0 +1,25 @@
+package test
+
+import (
+	"testing"
+
+	yaac_backend_mail "github.com/DHBW-SE-2023/YAAC/internal/backend/mail"
+	yaac_mvvm "github.com/DHBW-SE-2023/YAAC/internal/mvvm"
+)
+
+func setupMail() (*yaac_backend_mail.BackendMail, error) {
+	mvvm := yaac_mvvm.New()
+	return yaac_backend_mail.New(mvvm, "sgf", "jhsdf", "sfsdf")
+}
+
+func TestCheckMailConnection(t *testing.T) {
+	mail_backend, err := setupMail()
+	if err != nil {
+		t.Fatalf("Error setup Mail client: Is the server online and the login data correkt? Error: %v", err)
+	}
+
+	if !mail_backend.CheckMailConnection() {
+		t.Fatal("Mail Connection ist not active")
+	}
+
+}
