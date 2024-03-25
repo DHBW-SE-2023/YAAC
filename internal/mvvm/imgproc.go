7ae3cd66e654cb871ebdd049a098e6276a64f39f
Leander Gmeiner <leander.gmeiner@gmail.com>
2024-03-25 20:59:11 +0100

    TODO: Learn how to use git

--- internal/mvvm/imgproc.go
+++ internal/mvvm/imgproc.go
@@ -4,22 +4,7 @@ import (
 	imgproc "github.com/DHBW-SE-2023/YAAC/internal/backend/imgproc"
 )
 
-<<<<<<< Updated upstream
 func (m *MVVM) ImgprocBackendStart() {
 	m.BackendImgproc = imgproc.NewBackend(m)
 	m.NewTable([]byte{})
-=======
-func (m *MVVM) ValidateTable(img []byte) {
-	b := imgproc.NewBackend(m)
-	f := frontend.New(m)
-
-	go func() {
-		table, err := b.ValidateTable(img)
-		if err != nil {
-			log.Println("backend.ValidateTable: ", err)
-		}
-
-		f.ReceiveNewTable(table)
-	}()
->>>>>>> Stashed changes
 }
